a69e9a7c3b9ab3a8f83e3f6788b6cd70a1916d37
Dakota Malchow <dakotamalchow@gmail.com>
2020-11-15 23:26:34 -0500

    Add views

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const app = express();
+const path = require("path");
+
+app.set("views",path.join(__dirname,"views"));
+app.set("view engine","ejs");
 
 app.get("/",(req,res)=>{
-    res.send("home page");
+    res.render("index");
 });
 
 app.listen(3000,()=>{
