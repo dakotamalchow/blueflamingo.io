f3ffe0e30ddf356b1fe152b4ec5985426f5c7da0
Dakota Malchow <dakotamalchow@gmail.com>
2020-11-16 23:33:05 -0500

    Add mongo

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost:27017/blueflamingo",{useNewUrlParser:true,useUnifiedTopology:true})
+    .then(() => {
+        console.log("Mongo connection open");
+    })
+    .catch(error => {
+        console.log("Error connecting to Mongo: " + error);
+    });
 
 app.set("views",path.join(__dirname,"views"));
 app.set("view engine","ejs");
