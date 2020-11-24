a6766481bdd45bb2d4be5ee16d9b3fe0adf3b0c7
Dakota Malchow <dakotamalchow@gmail.com>
2020-11-23 22:50:57 -0500

    Add stripe

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const mongoose = require("mongoose");
+const stripe = require("stripe")('sk_test_F7a54OYuDnabmUT6HN2pLiDu');
 
 const Product = require("./models/product");
+
+const domain = 'http://localhost:3030';
 
 mongoose.connect("mongodb://localhost:27017/blueflamingo",{useNewUrlParser:true,useUnifiedTopology:true})
     .then(() => {
@@ -24,6 +27,29 @@ app.get("/",(req,res)=>{
 app.get("/products",async (req,res)=>{
     const products = await Product.find({});
     res.render("products",{products});
+});
+
+app.post('/create-session', async (req, res) => {
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ['card'],
+    line_items: [
+      {
+        price_data: {
+          currency: 'usd',
+          product_data: {
+            name: 'Test Item',
+            //images: ['https://i.imgur.com/EHyR2nP.png'],
+          },
+          unit_amount: 2000,
+        },
+        quantity: 1,
+      },
+    ],
+    mode: 'payment',
+    success_url: `${domain}/success.html`,
+    cancel_url: `${domain}/cancel.html`,
+  });
+  res.json({ id: session.id });
 });
 
 app.listen(3000,()=>{
