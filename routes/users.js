416843c406bbb253670ca7536b44de2d2c194b18
Dakota Malchow <dakotamalchow@gmail.com>
2020-12-02 23:07:28 -0500

    Add user logout

--- routes/users.js
+++ routes/users.js
@@ -20,4 +20,9 @@ router.post("/login",passport.authenticate("local"),(req,res)=>{
     res.redirect("/");
 });
 
+router.get("/logout",(req,res)=>{
+    req.logout();
+    res.redirect("/");
+});
+
 module.exports = router;
