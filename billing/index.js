3e20f13e64583963cca945434b2442546675a67a
Dakota Malchow <dakotamalchow@gmail.com>
2021-01-16 11:26:31 -0500

    Create template

--- billing/index.js
+++ billing/index.js
@@ -1,5 +1,14 @@
 const express = require("express");
 const app = express();
+const path = require("path");
+/*
+app.set("views",path.join(__dirname,"views"));
+*/
+app.set("view engine","ejs");
+
+app.get("/",(req,res)=>{
+    res.render("index");
+});
 
 app.listen(3000,()=>{
     console.log("App is listening on Port 3000");
