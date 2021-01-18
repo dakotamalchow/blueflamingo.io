6dfaa1fd4af013b997474156ac8711b7b69ce83b
Dakota Malchow <dakotamalchow@gmail.com>
2021-01-17 20:28:28 -0500

    Create request payment form

--- billing/index.js
+++ billing/index.js
@@ -1,13 +1,21 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-/*
+
 app.set("views",path.join(__dirname,"views"));
-*/
 app.set("view engine","ejs");
 
 app.get("/",(req,res)=>{
     res.render("index");
+});
+
+app.get("/request-payment",(req,res)=>{
+    res.render("request-payment");
+});
+
+app.post("/request-payment",(req,res)=>{
+    const {name,email,amount,notes} = req.body;
+    res.redirect("/");
 });
 
 app.listen(3000,()=>{
