e10c4ba9639b927464adf3e1bf65f6ce27c6f6b6
Dakota Malchow <dakotamalchow@gmail.com>
2021-01-17 21:36:36 -0500

    Show payments

--- billing/index.js
+++ billing/index.js
@@ -21,8 +21,9 @@ app.get("/",(req,res)=>{
     res.render("index");
 });
 
-app.get("/payments",(req,res)=>{
-    res.render("payments");
+app.get("/payments",async(req,res)=>{
+    const payments = await Payment.find({})
+    res.render("payments",{payments});
 });
 
 app.get("/request-payment",(req,res)=>{
@@ -32,6 +33,9 @@ app.get("/request-payment",(req,res)=>{
 app.post("/request-payment",async(req,res)=>{
     const {name,email,amount,notes} = req.body;
     const payment = new Payment({name,email,amount,notes});
+    payment.save((err)=>{
+        console.log(err);
+    });
     res.redirect("/payments");
 });
 
