10d1f40c4cff57b525dbf577cacde12d8bf1e18f
Dakota Malchow <dakotamalchow@gmail.com>
2021-01-29 12:17:37 -0500

    Move routes to separate file

--- index.js
+++ index.js
@@ -4,15 +4,12 @@ const path = require("path");
 const methodOverride = require("method-override");
 const mongoose = require("mongoose");
 const ejsMate = require("ejs-mate");
-const stripe = require("stripe")("sk_test_F7a54OYuDnabmUT6HN2pLiDu");
-const nodemailer = require("nodemailer");
-const aws = require("aws-sdk");
-const catchAsync = require("./utils/catchAsync");
 
-const Invoice = require("./models/invoice");
 const AppError = require("./utils/AppError");
+const invoiceRoutes = require("./routes/invoices");
 
-mongoose.connect("mongodb://localhost:27017/blueflamingo",{useNewUrlParser:true,useUnifiedTopology:true})
+mongoose.connect("mongodb://localhost:27017/blueflamingo",
+    {useNewUrlParser:true,useUnifiedTopology:true})
     .then(() => {
         console.log("Mongo connection open");
     })
@@ -33,102 +30,11 @@ app.use("/jquery-js", express.static(path.join(__dirname,"node_modules/jquery/di
 app.use(express.urlencoded({extended:true}))
 app.use(express.json());
 app.use(methodOverride("_method"));
-
-aws.config.loadFromPath(path.join(__dirname,"aws-config.json"));
-
-const transporter = nodemailer.createTransport({
-    SES: new aws.SES({
-        apiVersion: "2010-12-01"
-    })
-});
+app.use("/invoices",invoiceRoutes);
 
 app.get("/",(req,res)=>{
     res.render("index");
 });
-
-app.get("/invoices",catchAsync(async(req,res)=>{
-    const invoices = await Invoice.find({})
-    res.render("billing/index",{invoices});
-}));
-
-app.get("/invoices/new",(req,res)=>{
-    res.render("billing/new");
-});
-
-app.post("/invoices",catchAsync(async(req,res)=>{
-    const {name,email,amount,notes} = req.body;
-    const status = "SENT";
-    const invoice = new Invoice({name,email,amount,notes,status});
-    invoice.save((err)=>{
-        if(err){
-            console.log("error:",err);
-        }
-    });
-
-    const mailOptions = {
-        from: "billing@blueflamingo.io",
-        to: "dakotamalchow@blueflamingo.io",
-        subject: "Invoice",
-        text: "This is your invoice. Follow the link to pay now: http://localhost:3000/invoices/"+invoice._id+"/pay",
-        html: "This is your invoice. <br><a href='http://localhost:3000/invoices/"+invoice._id+"/pay'>Pay now.</a>"
-    }
-    transporter.sendMail(mailOptions,(err,info)=>{
-        if(err){
-            console.log("Error: ",err);
-        } else {
-            console.log("Email sent: ",info.response);
-        }
-    });
-    res.redirect("/invoices");
-}));
-
-app.get("/invoices/:id/pay",catchAsync(async(req,res)=>{
-    const invoiceId = req.params.id;
-    const invoice = await Invoice.findById(invoiceId);
-    res.render("billing/pay",{invoice});
-}));
-
-app.post("/invoices/:id/pay",catchAsync(async(req,res)=>{
-    const {amount} = req.body;
-    const paymentIntent = await stripe.paymentIntents.create({
-        amount: amount*100,
-        currency: "usd"
-    });
-    res.send({
-        clientSecret: paymentIntent.client_secret
-    });
-}));
-
-app.put("/invoices/:id/update",catchAsync(async(req,res)=>{
-    const invoiceId = req.params.id;
-    const invoice = await Invoice.findById(invoiceId);
-    invoice.status = "PAID";
-    invoice.save((err)=>{
-        if(err){
-            console.log("error:",err);
-        }
-    });
-    const mailOptions = {
-        from: "billing@blueflamingo.io",
-        to: "dakotamalchow@blueflamingo.io",
-        subject: "Invoice Paid",
-        text: "Your invoice has been paid. This is a receipt for your records.",
-        html: "Your invoice has been paid. This is a receipt for your records."
-    }
-    transporter.sendMail(mailOptions,(err,info)=>{
-        if(err){
-            console.log("Error: ",err);
-        } else {
-            console.log("Email sent: ",info.response);
-        }
-    });
-    // res.redirect(303,"/invoices");
-}));
-
-app.delete("/invoices",catchAsync(async(req,res)=>{
-    const deleted = await Invoice.deleteMany({});
-    res.redirect("/invoices");
-}));
 
 app.all("*",(req,res,next)=>{
     next(new AppError(404,"Page not found"));
---
+++ routes/invoices.js
@@ -0,0 +1,104 @@
+const express = require("express");
+const router = express.Router({mergeParams:true});
+const path = require("path");
+const stripe = require("stripe")("sk_test_F7a54OYuDnabmUT6HN2pLiDu");
+const nodemailer = require("nodemailer");
+const aws = require("aws-sdk");
+
+const catchAsync = require("../utils/catchAsync");
+const Invoice = require("../models/invoice");
+
+aws.config.loadFromPath(path.join(__dirname,"../aws-config.json"));
+
+const transporter = nodemailer.createTransport({
+    SES: new aws.SES({
+        apiVersion: "2010-12-01"
+    })
+});
+
+router.get("/",catchAsync(async(req,res)=>{
+    const invoices = await Invoice.find({})
+    res.render("billing/index",{invoices});
+}));
+
+router.get("/new",(req,res)=>{
+    res.render("billing/new");
+});
+
+router.post("/",catchAsync(async(req,res)=>{
+    console.log(req.body);
+    const {name,email,amount,notes} = req.body;
+    const status = "SENT";
+    const invoice = new Invoice({name,email,amount,notes,status});
+    invoice.save((err)=>{
+        if(err){
+            console.log("error:",err);
+        }
+    });
+
+    const mailOptions = {
+        from: "billing@blueflamingo.io",
+        to: "dakotamalchow@blueflamingo.io",
+        subject: "Invoice",
+        text: "This is your invoice. Follow the link to pay now: http://localhost:3000/invoices/"+invoice._id+"/pay",
+        html: "This is your invoice. <br><a href='http://localhost:3000/invoices/"+invoice._id+"/pay'>Pay now.</a>"
+    }
+    transporter.sendMail(mailOptions,(err,info)=>{
+        if(err){
+            console.log("Error: ",err);
+        } else {
+            console.log("Email sent: ",info.response);
+        }
+    });
+    res.redirect("/invoices");
+}));
+
+router.get("/:id/pay",catchAsync(async(req,res)=>{
+    const invoiceId = req.params.id;
+    const invoice = await Invoice.findById(invoiceId);
+    res.render("billing/pay",{invoice});
+}));
+
+router.post("/:id/pay",catchAsync(async(req,res)=>{
+    const {amount} = req.body;
+    const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount*100,
+        currency: "usd"
+    });
+    res.send({
+        clientSecret: paymentIntent.client_secret
+    });
+}));
+
+router.put("/:id/update",catchAsync(async(req,res)=>{
+    const invoiceId = req.params.id;
+    const invoice = await Invoice.findById(invoiceId);
+    invoice.status = "PAID";
+    invoice.save((err)=>{
+        if(err){
+            console.log("error:",err);
+        }
+    });
+    const mailOptions = {
+        from: "billing@blueflamingo.io",
+        to: "dakotamalchow@blueflamingo.io",
+        subject: "Invoice Paid",
+        text: "Your invoice has been paid. This is a receipt for your records.",
+        html: "Your invoice has been paid. This is a receipt for your records."
+    }
+    transporter.sendMail(mailOptions,(err,info)=>{
+        if(err){
+            console.log("Error: ",err);
+        } else {
+            console.log("Email sent: ",info.response);
+        }
+    });
+    // res.redirect(303,"/invoices");
+}));
+
+router.delete("/",catchAsync(async(req,res)=>{
+    const deleted = await Invoice.deleteMany({});
+    res.redirect("/invoices");
+}));
+
+module.exports = router;
