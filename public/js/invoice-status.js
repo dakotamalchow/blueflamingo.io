5a38637c16e0e4313d3f0576635a9fc396608c40
Dakota Malchow <dakotamalchow@gmail.com>
2021-02-10 16:36:44 -0500

    Change color depending on status

---
+++ public/js/invoice-status.js
@@ -0,0 +1,19 @@
+let backgroundColor = "";
+const invoiceStatusSpan = document.querySelector("#invoice-status");
+console.log(invoiceStatusSpan.innerText)
+
+switch(invoiceStatusSpan.innerText){
+    case "draft":
+        backgroundColor += "bg-secondary";
+        break;
+    case "open":
+        backgroundColor += "bg-success";
+        break;
+    case "paid":
+        backgroundColor += "bg-primary";
+        break;
+    default:
+        backgroundColor += "bg-danger"
+};
+
+invoiceStatusSpan.classList.add(backgroundColor);
