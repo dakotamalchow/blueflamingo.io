6f3fd36c9835f3e2d0184a7fe7ca688911f96ee6
Dakota Malchow <dakotamalchow@gmail.com>
2021-02-12 23:56:26 -0500

    Send email when invoice is created, refactor to controller class

---
+++ controllers/invoices.js
@@ -0,0 +1,75 @@
+const fs = require("fs");
+const ejs = require("ejs");
+const stripe = require('stripe')('sk_test_F7a54OYuDnabmUT6HN2pLiDu');
+const sgMail = require("@sendgrid/mail");
+require("dotenv").config();
+
+const Invoice = require("../models/invoice");
+const Customer = require("../models/customer");
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+module.exports.createInvoiceDraft = async(customerId,lineItems,notes,user)=>{
+    const customer = await Customer.findById(customerId);
+    const invoiceCount = user.increaseInvoiceCount();
+    const invoiceNumber = String(invoiceCount).padStart(4,"0");
+    const invoice = new Invoice({user,customer,invoiceNumber});
+    //lineItems comes back as nested objects, so this returns an array
+    for(let item of Object.values(lineItems)){
+        const stripeAmount = parseFloat(item.amount)*100;
+        await stripe.invoiceItems.create({
+            customer: customer.stripeCustomer,
+            amount: stripeAmount,
+            currency: "usd",
+            description: item.description
+        });
+    };
+    const stripeInvoice = await stripe.invoices.create({
+        customer: customer.stripeCustomer,
+        transfer_data:{
+            destination:user.stripeAccount
+        },
+        description: notes,
+        collection_method: "send_invoice",
+        days_until_due: 30,
+        metadata:{
+            invoiceId: invoice.id,
+            invoiceNumber: invoiceNumber,
+            userName: user.businessName
+        }
+    });
+    invoice.stripeInvoice = stripeInvoice.id;
+    await invoice.save();
+};
+
+module.exports.sendEmailInvoice = async(invoiceId)=>{
+    const invoice = await Invoice.findById(invoiceId);
+    const stripeInvoice = await stripe.invoices.retrieve(invoice.stripeInvoice);
+    const invoiceTemplate = fs.readFileSync("views/email/invoice.ejs",{encoding:"utf-8"});
+    let statusColor = "";
+    switch(stripeInvoice.status){
+        case "draft":
+            //secondary - grey
+            statusColor += "#6c757d";
+            break;
+        case "open":
+            //success - green
+            statusColor += "#28a745";
+            break;
+        case "paid":
+            //primary - blue
+            statusColor += "#0040F0";
+            break;
+        default:
+            //danger - red
+            statusColor += "#dc3545"
+    };
+    const msg = {
+        to:"dakotamalchow@gmail.com",
+        from:"billing@blueflamingo.io",
+        subject:`New Invoice from ${stripeInvoice.metadata.userName}`,
+        text:`${stripeInvoice.metadata.userName} sent you a new invoice for $${(stripeInvoice.amount_due/100).toFixed(2)}. Please visit blueflamingo.io/invoices/${stripeInvoice.metadata.invoiceId}/pay to pay your invoice.`,
+        html:ejs.render(invoiceTemplate,{stripeInvoice,statusColor})
+    };
+    await sgMail.send(msg);
+};
--- routes/invoices.js
+++ routes/invoices.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const router = express.Router({mergeParams:true});
 const path = require("path");
-const fs = require("fs");
-const ejs = require("ejs");
 const stripe = require('stripe')('sk_test_F7a54OYuDnabmUT6HN2pLiDu');
 const aws = require("aws-sdk");
 const sgMail = require("@sendgrid/mail");
@@ -12,6 +10,7 @@ const catchAsync = require("../utils/catchAsync");
 const Invoice = require("../models/invoice");
 const Customer = require("../models/customer");
 const {isLoggedIn,validateInvoiceReqBody} = require("../utils/middleware");
+const {createInvoiceDraft,sendEmailInvoice} = require("../controllers/invoices");
 
 aws.config.loadFromPath(path.join(__dirname,"../aws-config.json"));
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
@@ -39,38 +38,9 @@ router.get("/new",isLoggedIn,catchAsync(async(req,res)=>{
 router.post("/",isLoggedIn,validateInvoiceReqBody,catchAsync(async(req,res)=>{
     const {customerId,lineItems,notes} = req.body;
     const user = res.locals.currentUser;
-    const customer = await Customer.findById(customerId);
-    
-    const invoiceCount = user.increaseInvoiceCount();
-    const invoiceNumber = String(invoiceCount).padStart(4,"0");
-    const invoice = new Invoice({user,customer,invoiceNumber});
-    //lineItems comes back as nested objects, so this returns an array
-    for(let item of Object.values(lineItems)){
-        const stripeAmount = parseFloat(item.amount)*100;
-        await stripe.invoiceItems.create({
-            customer: customer.stripeCustomer,
-            amount: stripeAmount,
-            currency: "usd",
-            description: item.description
-        });
-    };
-    const stripeInvoice = await stripe.invoices.create({
-        customer: customer.stripeCustomer,
-        transfer_data:{
-            destination:user.stripeAccount
-        },
-        description: notes,
-        collection_method: "send_invoice",
-        days_until_due: 30,
-        metadata:{
-            invoiceId: invoice.id,
-            invoiceNumber: invoiceNumber
-        }
-    });
-    invoice.stripeInvoice = stripeInvoice.id;
-    await invoice.save();
-
-    await stripe.invoices.sendInvoice(invoice.stripeInvoice);
+    await createInvoiceDraft(customerId,lineItems,notes,user);
+    await stripe.invoices.finalizeInvoice(stripeInvoice.id);
+    await sendEmailInvoice(invoice._id);
     req.flash("success","Successfully created and sent invoice");
     res.redirect("/invoices");
 }));
@@ -83,77 +53,16 @@ router.get("/:id",isLoggedIn,catchAsync(async(req,res)=>{
 }));
 
 router.post("/:id/send",async(req,res)=>{
-    const invoiceId = req.params.id;
-    // const user = res.locals.currentUser;
-    // const userName = user.businessName || user.name;
-    const userName = "Richard's Construction";
-    const invoice = await Invoice.findById(invoiceId);
-    const stripeInvoice = await stripe.invoices.retrieve(invoice.stripeInvoice);
-    const invoiceTemplate = fs.readFileSync("views/email/invoice.ejs",{encoding:"utf-8"});
-    let statusColor = "";
-    switch(stripeInvoice.status){
-        case "draft":
-            //secondary - grey
-            statusColor += "#6c757d";
-            break;
-        case "open":
-            //success - green
-            statusColor += "#28a745";
-            break;
-        case "paid":
-            //primary - blue
-            statusColor += "#0040F0";
-            break;
-        default:
-            //danger - red
-            statusColor += "#dc3545"
-    };
-    const msg = {
-        to:"dakotamalchow@gmail.com",
-        from:"billing@blueflamingo.io",
-        subject:`New Invoice from ${userName}`,
-        text:`${userName} sent you a new invoice for $${(stripeInvoice.amount_due/100).toFixed(2)}. Please visit blueflamingo.io/invoices/${stripeInvoice.metadata.invoiceId}/pay to pay your invoice.`,
-        html:ejs.render(invoiceTemplate,{stripeInvoice,userName,statusColor})
-    };
-    await sgMail.send(msg);
+    sendEmailInvoice(req.params.id);
     res.send("Email sent");
 });
 
-router.get("/:id/test",async(req,res)=>{
-    const invoiceId = req.params.id;
-    // const user = res.locals.currentUser;
-    // const userName = user.businessName || user.name;
-    const userName = "Richard's Construction";
-    const invoice = await Invoice.findById(invoiceId);
-    const stripeInvoice = await stripe.invoices.retrieve(invoice.stripeInvoice);
-    const invoiceTemplate = fs.readFileSync("views/email/invoice.ejs",{encoding:"utf-8"});
-    let statusColor = "";
-    switch(stripeInvoice.status){
-        case "draft":
-            //secondary - grey
-            statusColor += "#6c757d";
-            break;
-        case "open":
-            //success - green
-            statusColor += "#28a745";
-            break;
-        case "paid":
-            //primary - blue
-            statusColor += "#0040F0";
-            break;
-        default:
-            //danger - red
-            statusColor += "#dc3545"
-    };
-    res.send(ejs.render(invoiceTemplate,{stripeInvoice,userName,statusColor}));
-});
-
 router.get("/:id/pay",async(req,res)=>{
+    console.log(req.baseUrl);
     const invoiceId = req.params.id;
     const invoice = await Invoice.findById(invoiceId);
     const stripeInvoice = await stripe.invoices.retrieve(invoice.stripeInvoice);
-    //need to add userName to stripeInvoice metadata and retreive it from there
-    userName = "Richard's Construction";
+    const userName = stripeInvoice.metadata.userName;
     res.render("billing/pay",{stripeInvoice,userName});
 });
 
