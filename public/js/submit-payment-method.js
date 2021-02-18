d54f1841a3aff6e398fbf6f4d130489c24b42342
Dakota Malchow <dakotamalchow@gmail.com>
2021-02-18 18:41:50 -0500

    Break apart card field and add postal code

--- public/js/submit-payment-method.js
+++ public/js/submit-payment-method.js
@@ -1,9 +1,7 @@
 const stripe = Stripe("pk_test_osZfCwd1uI7FjnfaUqWxbu2R");
 const elements = stripe.elements();
 
-const card = elements.create("card");
-card.mount("#card-element");
-card.on("change",({error})=>{
+const setDisplayError = ({error})=>{
     let displayError = document.querySelector("#card-errors");
     if(error){
         displayError.textContent = error.message;
@@ -11,14 +9,30 @@ card.on("change",({error})=>{
     else{
         displayError.textContent = "";
     };
-});
+}
+
+const cardNumberElement = elements.create('cardNumber');
+cardNumberElement.mount("#cardNumber");
+cardNumberElement.on("change",setDisplayError);
+const cardExpiryElement = elements.create('cardExpiry');
+cardExpiryElement.mount("#cardExpiry");
+cardExpiryElement.on("change",setDisplayError);
+const cardCvcElement = elements.create('cardCvc');
+cardCvcElement.mount("#cardCvc");
+cardCvcElement.on("change",setDisplayError);
 
 const form = document.querySelector("#payment-form");
 form.addEventListener("submit",async(event)=>{
     event.preventDefault();
+    const postalCode = document.querySelector("#postalCode").value;
     const {paymentMethod,error} = await stripe.createPaymentMethod({
         type:"card",
-        card:card
+        card:cardNumberElement,
+        billing_details:{
+            address:{
+                postal_code: postalCode
+            }
+        }
     });
     if (error){
         const errorElement = document.querySelector("#card-errors");
