c7fcbe278ab26ec176bf4d202557f82775489845
Dakota Malchow <dakotamalchow@gmail.com>
2021-02-27 14:02:50 -0500

    Show admin registered users

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,3 +1,10 @@
+const User = require("../models/user");
+
 module.exports.index = (req,res)=>{
     res.render("admin/index");
+};
+
+module.exports.usersIndex = async(req,res)=>{
+    const users = await User.find();
+    res.render("admin/users/index",{users});
 };
--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const router = express.Router({mergeParams:true});
 
+const catchAsync = require("../utils/catchAsync");
 const {isLoggedIn,isAdmin} = require("../utils/middleware");
 const admin = require("../controllers/admin");
 
 router.get("/",isLoggedIn,isAdmin,admin.index);
+
+router.get("/users",isLoggedIn,isAdmin,catchAsync(admin.usersIndex));
 
 module.exports = router;
