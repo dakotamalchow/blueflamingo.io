1ada7e1b6bd7c9bf67d42263c75994fcee0cae96
Dakota Malchow <dakotamalchow@gmail.com>
2021-03-11 16:16:58 -0500

    Create index and edit of items

--- controllers/items.js
+++ controllers/items.js
@@ -1,5 +1,12 @@
 const Item = require("../models/item");
 
+module.exports.index = async(req,res)=>{
+    const user = res.locals.currentUser;
+    req.session.returnTo = req.originalUrl;
+    const items = await Item.find({user});
+    res.render("items/index",{items});
+};
+
 module.exports.newForm = async(req,res)=>{
     const returnToUrl = req.session.returnTo || "/items";
     delete req.session.returnTo;
@@ -13,4 +20,24 @@ module.exports.saveItem = async(req,res)=>{
     await item.save();
     req.flash("success","Successfully saved new item");
     res.redirect(returnToUrl);
+};
+
+module.exports.itemDetails = async(req,res)=>{
+    const itemId = req.params.id;
+    const item = await Item.findById(itemId);
+    res.render("items/details",{item});
+};
+
+module.exports.editForm = async(req,res)=>{
+    const itemId = req.params.id;
+    const item = await Item.findById(itemId);
+    res.render("items/edit",{item});
+};
+
+module.exports.editItem = async(req,res)=>{
+    const itemId = req.params.id;
+    const {description,amount} = req.body;
+    const item = await Item.findByIdAndUpdate(itemId,{description,amount},{new:true});
+    req.flash("success","Item successfully saved");
+    res.redirect(`/items/${item._id}`);
 };
--- routes/items.js
+++ routes/items.js
@@ -5,8 +5,16 @@ const catchAsync = require("../utils/catchAsync");
 const {isLoggedIn,isStripeVerified,hasPlan} = require("../utils/middleware");
 const items = require("../controllers/items");
 
+router.get("/",isLoggedIn,isStripeVerified,hasPlan,catchAsync(items.index));
+
 router.get("/new",isLoggedIn,isStripeVerified,hasPlan,catchAsync(items.newForm));
 
 router.post("/",isLoggedIn,isStripeVerified,hasPlan,catchAsync(items.saveItem));
+
+router.get("/:id",isLoggedIn,isStripeVerified,hasPlan,catchAsync(items.itemDetails));
+
+router.get("/:id/edit",isLoggedIn,isStripeVerified,hasPlan,catchAsync(items.editForm));
+
+router.post("/:id",isLoggedIn,isStripeVerified,hasPlan,catchAsync(items.editItem));
 
 module.exports = router;
